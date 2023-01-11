e90a17002fc7606ce5afec00a62553d5d26b7273
vinaysrihari93 <vinaysrihari93@gmail.com>
2023-01-11 12:50:39 +0530

    Testing new function

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def func_1():
+    print('Branch')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -15,5 +17,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print('Edit')
     print('Hi')
+    print(func_1())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
